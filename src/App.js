b8914d91270f406526ef285d142b97fa3a0bdc6c
Deyvid-10 <marmolejodeyvid@gmail.com>
2024-03-14 21:29:34 -0400

    .

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Cabecera from './componentes_js/Cabecera';
+import Inicio from './componentes_js/Inicio';
+import './App.css';
+
+function App() {
+  return (
+      <div>
+        <Cabecera/>
+        <Inicio/>
+      </div>
+  );
+}
+
+export default App;
