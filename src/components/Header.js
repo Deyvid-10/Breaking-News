dee0aa0d0a799bce5dd62756c21b808fd97bce19
Deyvid-10 <marmolejodeyvid@gmail.com>
2024-04-22 23:40:54 -0400

    .

--- src/components/Header.js
+++ src/components/Header.js
@@ -26,127 +26,127 @@ function Header() {
   console.log(getTokenUser);
   // console.log(tokenUser);
 
-  // let [userName, setUserName] = useState("")
-  // let [userAvatar, setUserAvatar] = useState("")
+  let [userName, setUserName] = useState("")
+  let [userAvatar, setUserAvatar] = useState("")
 
-  // const getUserDates = () =>
-  // {
-  //   if(getTokenUser !== "")
-  //   {
-  //     fetch(`${all.link}/user/${tokenUser[1]}`,{
-  //       method: "GET",
-  //       headers: {
-  //         "Content-Type": "application/json"
-  //       }
-  //     })
-  //     .then(respuesta => respuesta.json())
-  //     .then(data=> {
-  //       setUserName(data[0].user)
-  //       setUserAvatar(data[0].avatar)
-  //     })
-  //     .catch(error => {throw new Error ("Error  en la solicitud: " + error)})
-  //   }
-  // }
+  const getUserDates = () =>
+  {
+    if(getTokenUser !== "")
+    {
+      fetch(`${all.link}/user/${tokenUser[1]}`,{
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json"
+        }
+      })
+      .then(respuesta => respuesta.json())
+      .then(data=> {
+        setUserName(data[0].user)
+        setUserAvatar(data[0].avatar)
+      })
+      .catch(error => {throw new Error ("Error  en la solicitud: " + error)})
+    }
+  }
 
-  // useEffect(getUserDates, [])
+  useEffect(getUserDates, [])
 
-  // // control the display user bar
-  // const [showUserBar, setShowUserBar] = useState(true);
+  // control the display user bar
+  const [showUserBar, setShowUserBar] = useState(true);
 
-  // const handleShowUserBar = () => 
-  // {
-  //   if(getTokenUser !== "")
-  //   {
-  //     setShowUserBar(false);
-  //   }
-  //   else
-  //   {
-  //     setShowUserBar(true)
-  //   }
-  // };
+  const handleShowUserBar = () => 
+  {
+    if(getTokenUser !== "")
+    {
+      setShowUserBar(false);
+    }
+    else
+    {
+      setShowUserBar(true)
+    }
+  };
 
-  // useEffect(handleShowUserBar, [])
+  useEffect(handleShowUserBar, [])
 
-  // // redirect to home page
-  // let nav = useNavigate()
-  // const rePublish = () =>
-  // {
-  //   nav("/publish")
-  // }
+  // redirect to home page
+  let nav = useNavigate()
+  const rePublish = () =>
+  {
+    nav("/publish")
+  }
 
-  // // get the input search value from header
+  // get the input search value from header
 
-  // const [searchValue, setSearchValue] = useState("")
+  const [searchValue, setSearchValue] = useState("")
 
-  // const getSearchValue = (event) =>
-  // {
-  //   setSearchValue(event.target.value)
-  // }
+  const getSearchValue = (event) =>
+  {
+    setSearchValue(event.target.value)
+  }
 
-  // // redirect to search page
-  // let search = useNavigate()
-  // const reSearch = () =>
-  // {
-  //   search(`/search/${searchValue}`)
-  //   window.location.reload()
-  // }
+  // redirect to search page
+  let search = useNavigate()
+  const reSearch = () =>
+  {
+    search(`/search/${searchValue}`)
+    window.location.reload()
+  }
 
-  // // validate the expiration of the token
-  // const validateTokenExpiration = () =>
-  // {
-  //   if(getTokenUser !== "")
-  //   {
-  //     fetch(`${all.link}/tokenExpiration`, {
-  //       method: 'POST',
-  //       headers: {
-  //           "Content-Type": "application/json"
-  //       },
-  //       body: JSON.stringify({
-  //           token: tokenUser[0]
-  //       })
-  //     })
-  //     .then(response => response.text())
-  //     .then(data => {
+  // validate the expiration of the token
+  const validateTokenExpiration = () =>
+  {
+    if(getTokenUser !== "")
+    {
+      fetch(`${all.link}/tokenExpiration`, {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            token: tokenUser[0]
+        })
+      })
+      .then(response => response.text())
+      .then(data => {
 
-  //       if(data != "active")
-  //       {
-  //         all.notification("negative", "Tu sesion expiro, ingresa de nuevo")
-  //         setTimeout(logout(), 2500)  
-  //       }
+        if(data != "active")
+        {
+          all.notification("negative", "Tu sesion expiro, ingresa de nuevo")
+          setTimeout(logout(), 2500)  
+        }
           
-  //     })
-  //     .catch(error => { throw new Error("Error en la solicitud: " + error) })
-  //   }
-  // }
+      })
+      .catch(error => { throw new Error("Error en la solicitud: " + error) })
+    }
+  }
 
-  // useEffect(validateTokenExpiration, [])
+  useEffect(validateTokenExpiration, [])
 
-  // // user logout 
-  // const logout = () =>
-  // {
-  //   localStorage.setItem("tokenUser", "")
-  //   window.location.reload()
-  // }
+  // user logout 
+  const logout = () =>
+  {
+    localStorage.setItem("tokenUser", "")
+    window.location.reload()
+  }
 
-  // const [showLogout, setShowLogout] = useState(true)  
+  const [showLogout, setShowLogout] = useState(true)  
 
-  // const handelShowLogout = () =>
-  // {
-  //   setShowLogout(!showLogout)
-  // }
+  const handelShowLogout = () =>
+  {
+    setShowLogout(!showLogout)
+  }
 
-  // let [avatar, setAvatar] = useState([])
-  // let profile = []
-  // const chargeProfile = () =>
-  // {
-  //   profile.push(
-  //     <img src={require(`../img/profile/caja.png`)} alt="avatar" className='w-10 border-2 border-blue-700 rounded-full'/>
-  //   )
+  let [avatar, setAvatar] = useState([])
+  let profile = []
+  const chargeProfile = () =>
+  {
+    profile.push(
+      <img src={require(`../img/profile/caja.png`)} alt="avatar" className='w-10 border-2 border-blue-700 rounded-full'/>
+    )
 
-  //   setAvatar(profile)
-  // }
+    setAvatar(profile)
+  }
 
-  // useEffect(chargeProfile, [userAvatar])
+  useEffect(chargeProfile, [userAvatar])
 
   return (
 
@@ -169,7 +169,7 @@ function Header() {
             <li className='flex items-center'><NavLink className={isActive('/entertainment') ? 'border-blue-700 bg-blue-100 lg:bg-white w-full border-l-4 lg:border-l-0 lg:hover:border-l-0 lg:hover:border-b-4 lg:border-b-4 text-black px-2 py-2 lg:py-4 text-md font-medium' : 'text-gray-500 hover:bg-blue-100 lg:hover:bg-white w-full border-white border-l-4 lg:border-l-0 lg:hover:border-l-0 hover:border-l-4 hover:border-blue-600 lg:border-b-4 hover:text-black px-2 py-2 lg:py-4 text-md font-medium'} to="/entertainment">Entretenimiento</NavLink></li>
           </ul>
           
-          {/* <div className={menu ? "hidden lg:flex py-3" : " flex py-3"}>
+          <div className={menu ? "hidden lg:flex py-3" : " flex py-3"}>
             <input type="text" onChange={getSearchValue} placeholder='Buscar...' className='block rounded-md w-full border-0 py-1.5 px-4 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6'/>
             <button onClick={reSearch} className='flex items-center bg-blue-700 text-white ml-2 px-2 py-1 rounded-md hover:bg-blue-600 font-bold'>
           
@@ -178,10 +178,10 @@ function Header() {
               </svg>
             Buscar
             </button>
-          </div> */}
+          </div>
       </nav>
 
-      {/* <div className={showUserBar ? 'hidden' : 'flex sm:flex-row flex-col px-8 shadow-md bg-white justify-between py-2'}>
+      <div className={showUserBar ? 'hidden' : 'flex sm:flex-row flex-col px-8 shadow-md bg-white justify-between py-2'}>
 
         <div className='sm:flex items-end hidden'>
           <p className='text-xl mr-2 mb-2 text-white'>{userName}</p>
@@ -208,7 +208,7 @@ function Header() {
             <p className='hover:cursor-pointer hover:text-gray-600' onClick={logout}>Cerrar sesion</p>
           </div>
         </div>
-      </div> */}
+      </div>
       
       <div id="notification"></div>
     </div>
